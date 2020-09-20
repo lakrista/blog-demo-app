b921a8073b7f3adbc6215835d8d275aa2a589bbd
lakrista <lakrista_gm@gmail.com>
2020-09-20 23:21:08 +0300

    correct

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import logo from './../logo.png';
 
 const Header = () => {
-	return <header className='header'>
-		<div>
-		</div>
-		<div className="">
-			<img alt="logo" src={logo} />
-		</div>
-		<div>
-		</div>
-	</header>
+    return <header className='header'>
+        <div>
+        </div>
+        <div className="">
+            <img alt="logo" src={logo}/>
+        </div>
+        <div>
+        </div>
+    </header>
 };
 
 export default Header;
