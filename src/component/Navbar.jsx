3f907e69fcedad0288e1449c248d6503dcf350f0
lakrista <lakrista_gm@gmail.com>
2020-09-30 18:59:52 +0300

    add navlinks

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
 	return <nav className='nav justify-content-center'>
 		<ul>
-			<a href="/"><li>Home</li></a>
-			<a href="/blog"><li>Blog</li></a>
-			<a href="/news"><li>News</li></a>
-			<a href="/about"><li>About</li></a>
-			<a href="/contact"><li>Contact</li></a>
+			<NavLink to="/"><li>Home</li></NavLink>
+			<NavLink to="/blog"><li>Blog</li></NavLink>
+			<NavLink to="/news"><li>News</li></NavLink>
+			<NavLink to="/about"><li>About</li></NavLink>
+			<NavLink to="/contact"><li>Contact</li></NavLink>
 		</ul>
 	</nav>
 };
